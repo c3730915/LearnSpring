a7ec04565a581b05edfa901c5d289b925762e832
c3730915 <1023700418@qq.com>
2020-11-11 17:14:06 +0800

    springMvc的简单使用，修复了因为依赖版本不同而报错的bug

---
+++ src/main/java/com/iitcast/web/UserController.java
@@ -0,0 +1,14 @@
+package com.iitcast.web;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+    @RequestMapping("/quick")
+    public String save(){
+        System.out.println("save running....");
+        return "success.jsp";
+    }
+}
